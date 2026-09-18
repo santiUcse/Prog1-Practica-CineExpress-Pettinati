b266136a47be9bd7fc25900a0c16b57e2abaf488
santiUcse <santiipettinati2@gmail.com>
2026-09-18 10:20:17 -0300

    Entidad pelicula creada

---
+++ backend/CineExpressApi/Models/Pelicula.cs
@@ -0,0 +1,14 @@
+﻿namespace CineExpressApi.Models;
+
+public class Pelicula
+{
+    public int Id { get; set; }
+
+    public string Titulo { get; set; } = "";
+
+    public string Genero { get; set; } = "";
+
+    public int Duracion { get; set; }
+
+    public int CopiasDisponibles { get; set; }
+}
